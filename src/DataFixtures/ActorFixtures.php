a942c43f67aad29b830bcb113afd192008bd2765
unknown <bouzire.noureddine1@gmail.com>
2024-11-26 14:26:33 +0100

    execute the database

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ActorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+       $actor = new Actor();
+       $actor->setName('Christian Bale');
+       $manager->persist($actor);
+
+       $actor1 = new Actor();
+       $actor1->setName('Heath Ledger');
+       $manager->persist($actor1);
+
+       $actor2 = new Actor();
+       $actor2->setName('Robert Downey J');
+       $manager->persist($actor2);
+
+       $actor3 = new Actor();
+       $actor3->setName('Chris Evans');
+       $manager->persist($actor3);
+
+       $manager->flush();
+
+       $this-> addReference('actor_1', $actor)
+       $
+
+
+
+
+    }
+}
