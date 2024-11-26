8714e07e475635111ebe9c7f6375bdb70bf971e9
unknown <bouzire.noureddine1@gmail.com>
2024-11-26 14:32:04 +0100

    upsateing again

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -28,8 +28,10 @@ class ActorFixtures extends Fixture
 
        $manager->flush();
 
-       $this-> addReference('actor_1', $actor)
-       $
+       $this-> addReference('actor_1', $actor);
+       $this->addReference('actor_2',$actor1);
+       $this->addReference('actor_3',$actor2);
+       $this->addReference('actor_4',$actor3);
 
 
 
