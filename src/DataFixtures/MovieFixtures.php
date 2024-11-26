1fbe989978ef5c5e4bd558d63e5895037340b879
unknown <bouzire.noureddine1@gmail.com>
2024-11-26 14:27:28 +0100

    adding the fixtures for the movie entity

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -15,6 +15,17 @@ class MovieFixtures extends Fixture
         $movie->setReleaseYear(2008);
         $movie->setDescription('This is the description of the dark knight');
         $movie ->setImagePath('https://cdn.pixabay.com/photo/2024/06/22/16/55/ai-generated-8846672_1280.jpg');
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2 ->setTitle('Avengers:EndGame');
+        $movie2->setReleaseYear(2019);
+        $movie2->setDescription('This is the description of Avenger: end game');
+        $movie2 ->setImagePath('https://cdn.pixabay.com/photo/2020/06/02/05/25/tarantula-5249451_1280.png');
+        $manager->persist($movie2);
+
+        $manager->flush();
+
 
     }
 }
