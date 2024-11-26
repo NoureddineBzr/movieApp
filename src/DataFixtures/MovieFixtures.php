70533c8e4470a91f2b50514aa3279b375a0861e9
unknown <bouzire.noureddine1@gmail.com>
2024-11-26 13:58:53 +0100

    loading the data

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Movie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,11 @@ class MovieFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $movie = new Movie();
+        $movie ->setTitle('The dark knight');
+        $movie->setReleaseYear(2008);
+        $movie->setDescription('This is the description of the dark knight');
+        $movie ->setImagePath('https://cdn.pixabay.com/photo/2024/06/22/16/55/ai-generated-8846672_1280.jpg');
 
-        $manager->flush();
     }
 }
